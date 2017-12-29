655e5b95f9586790976ead2fe17569bb59fb4148
Wally <wolaondo@gmail.com>
2017-12-29 14:23:19 -0300

    adding classes with React.Component

--- src/app.js
+++ src/app.js
@@ -1,54 +1,51 @@
-console.log('App.js is running');
-
-const app = {
-    title: 'JSX !!',
-    subtitle: 'Awesome ReactJs',
-    options: []
-};
-
-const onFormSubmit = (e) => {
-    e.preventDefault();//stop page refresh
-    console.log('form submited');
-    const option = e.target.elements.option.value;
-    
-    if(option) {
-        app.options.push(option);
-        e.target.elements.option.value="";
-        renderCustom();
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>What If?? Sub-Header</h2>
+            </div>
+        );
     }
-    
-};
-const removeAll = () => {
-    app.options = [];    
-    renderCustom();
 }
 
-const makeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    const optionSelected = app.options[randomNum];
-    alert(optionSelected);
-    renderCustom();
-}
-const renderCustom = () => {
-    const template = (
-        <div> 
-            <p>{app.title}</p> 
-            {app.subtitle && <h1>{app.subtitle}</h1>}
-            <p>{app.options.length > 0 ? 'Here are your options' : 'No options' }</p>                           
-            <ol>{ 
-                    app.options.map((x) => <li key={x}>Item {x}</li>) 
-                }                
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option"/>
-                <button>Add Option</button>                
-            </form>
-            <button onClick={removeAll}>Remove All</button>
-            <button disabled={app.options.length < 1} onClick={makeDecision}>what should I do?</button>
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button> What should I do?</button>
+            </div>
+        );
+    }
 }
 
-const appRoot = document.getElementById('app');
-renderCustom();
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Options</p>
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>Add Option</button>
+            </div>
+        );
+    }
+}
+
+const jsx = (
+    <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/playground/oldJsxfirstApp.js
@@ -0,0 +1,54 @@
+console.log('App.js is running');
+
+const app = {
+    title: 'JSX !!',
+    subtitle: 'Awesome ReactJs',
+    options: []
+};
+
+const onFormSubmit = (e) => {
+    e.preventDefault();//stop page refresh
+    console.log('form submited');
+    const option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = "";
+        renderCustom();
+    }
+
+};
+const removeAll = () => {
+    app.options = [];
+    renderCustom();
+}
+
+const makeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const optionSelected = app.options[randomNum];
+    alert(optionSelected);
+    renderCustom();
+}
+const renderCustom = () => {
+    const template = (
+        <div>
+            <p>{app.title}</p>
+            {app.subtitle && <h1>{app.subtitle}</h1>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <ol>{
+                app.options.map((x) => <li key={x}>Item {x}</li>)
+            }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button>Add Option</button>
+            </form>
+            <button onClick={removeAll}>Remove All</button>
+            <button disabled={app.options.length < 1} onClick={makeDecision}>what should I do?</button>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+}
+
+const appRoot = document.getElementById('app');
+renderCustom();

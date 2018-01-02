b997cb368904825e67ee6e33456392e3bcc5494a
Wally <wolaondo@gmail.com>
2018-01-02 13:48:57 -0300

    adding VisibilityToggle with Component State to visiblejs

--- src/playground/visible.js
+++ src/playground/visible.js
@@ -1,21 +1,55 @@
-let visibility = false;
-
-const showToggle = () => {    
-    visibility = !visibility    
-    renderCustom();
-}
-
-const renderCustom = () => {
-    const template = (
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);        
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        };
+    }
+    handleToggleVisibility() {              
+        this.setState((prevState) => {           
+            return {                
+             visibility: !prevState.visibility
+            };            
+        });        
+    }
+    render() {
+        return(
         <div>
-            <h1><p>Visibility Toggle</p></h1>                    
-            <button onClick={showToggle}>{visibility ? 'Hide Details' : 'Show Details'}</button>
+            <h1><p>Visibility Toggle</p></h1>
+            <button onClick={this.handleToggleVisibility}>
+                {this.state.visibility ? 'Hide Details' : 'Show Details'}
+            </button>
 
-            {visibility && (<div>Detalles habia q escribir algo....</div>)}
+            {this.state.visibility && (<div>Detalles habia q escribir algo....</div>)}
         </div>
-    );
-    ReactDOM.render(template, appRoot);
+        )
+    };
 }
 
 const appRoot = document.getElementById('app');
-renderCustom();
+ReactDOM.render(<VisibilityToggle />, appRoot);
+
+
+//old code
+// let visibility = false;
+
+// const showToggle = () => {
+//     visibility = !visibility
+//     renderCustom();
+// }
+
+// const renderCustom = () => {
+//     const template = (
+//         <div>
+//             <h1><p>Visibility Toggle</p></h1>
+//             <button onClick={showToggle}>{visibility ? 'Hide Details' : 'Show Details'}</button>
+
+//             {visibility && (<div>Detalles habia q escribir algo....</div>)}
+//         </div>
+//     );
+//     ReactDOM.render(template, appRoot);
+// }
+
+// const appRoot = document.getElementById('app');
+// renderCustom();

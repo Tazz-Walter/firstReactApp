99ced9ff382bebafea24669d11c890f029c99b51
Wally <wolaondo@gmail.com>
2018-01-02 13:12:15 -0300

    component State with stateCounter from scrash

--- src/app.js
+++ src/app.js
@@ -26,19 +26,32 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('handlePick');
+    }
     render() {
         return (
             <div>
-                <button> What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
+    //override el constructor de Component asi no debemos llamar a bind() cada vez q se haga click
+    //de esta manera al redefinir su constructor estamos bindeando/ligando desde su constructor
+    constructor(props) { 
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
+    handleRemoveAll() {      
+        console.log(this.props.options);
+    }
     render() {
         return (
-            <div>            
+            <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
                 {
                     this.props.options.map((opt) => <Option key={opt} text={opt} />)
                 }
@@ -58,11 +71,29 @@ class Option extends React.Component {
     }
 }
 class AddOption extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.handleAddOption = this.handleAddOption.bind(this);
+    // }
+    handleAddOption(e) {
+        e.preventDefault();
+        const option = e.target.elements.option.value.trim();        
+        
+        if (option) {
+        //    this.props.options.push(option);
+           e.target.elements.option.value = "";                    
+        }
+        console.log(option);
+        // console.log(this.props.options);
+    }
     render() {
         return (
             <div>
-                <button>Add Option</button>
-            </div>
+                <form onSubmit={this.handleAddOption}>
+                    <input type="text" name="option" />
+                    <button>Add Option</button>
+                </form>
+            </div>            
         );
     }
 }
